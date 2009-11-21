d7a03f86c42bba12058491e8028918c250c35c0c
Nik <Nik@Westminster>
2009-11-21 21:38:26 +0000

    Added a bit of date management code.

---
+++ tests/test-Date-en-GB.js
@@ -0,0 +1,17 @@
+/**
+ * Test the Date-en-GB.js library from
+ * http://www.datejs.com
+ */
+
+testCases.push( function(Y) {
+
+	return new Y.Test.Case({
+
+		testISO8601ToDue: function() {
+			var utc_2009_12_01 = "2009-12-01T00:00:00Z";
+			Y.Assert.areEqual('Tue', Date.parse(utc_2009_12_01).toString('ddd'), "Bad MD5");
+		}
+
+	});
+
+} );
