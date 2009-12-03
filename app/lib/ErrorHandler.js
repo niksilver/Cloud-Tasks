f8d148f9c888856dad74fbd54f04830df3bb6bbc
Nik <Nik@Westminster>
2009-12-03 00:24:41 +0000

    Replace old name in ErrorHandler.

--- app/lib/ErrorHandler.js
+++ app/lib/ErrorHandler.js
@@ -1,6 +1,6 @@
 ErrorHandler = {
 	notify: function(err_msg) {
 		Mojo.Log.info("ErrorHandler.notify: Error: " + err_msg);
-		Mojo.Controller.getAppController().showBanner(err_msg, {}, 'org.pigsaw.simplertm');
+		Mojo.Controller.getAppController().showBanner(err_msg, {}, 'org.pigsaw.cloudtasks');
 	}
 }
