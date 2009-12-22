3b740b4b78c8e7034bd7702364152603abfbefaa
Nik <Nik@Westminster>
2009-12-22 23:00:01 +0000

    Introducing class EventSpacer to help space out push task events.

---
+++ app/lib/EventSpacer.js
@@ -0,0 +1,29 @@
+/**
+ * Class to help us space out events with a timer.
+ */
+
+/**
+ * Set up a new event spacer, defining the minimum spacing between events.
+ * Initially it will say it's ready to fire an event.
+ * @param {Number} spacing_ms  The spacing between events, in milliseconds.
+ */
+function EventSpacer(spacing_ms) {
+	this._spacing_ms = spacing_ms;
+	this._is_ready = true;
+}
+
+/**
+ * See if it's time enough to fire another event.
+ */
+EventSpacer.prototype.isReady = function() {
+	return this._is_ready;
+}
+
+EventSpacer.prototype.haveFired = function() {
+	this._is_ready = false;
+	var inst = this;
+	setTimeout(function() {
+			inst._is_ready = true;
+		},
+		this._spacing_ms);
+}
---
+++ tests/test-EventSpacer.js
@@ -0,0 +1,31 @@
+/**
+ * Test the class which spaces out events.
+ */
+
+testCases.push( function(Y) {
+
+	return new Y.Test.Case({
+
+		testSpacer: function() {
+			var spacer = new EventSpacer(100);
+			
+			Y.Assert.areEqual(true, spacer.isReady(), "Spacer should be ready for event after initialisation");
+			
+			spacer.haveFired();
+			Y.Assert.areEqual(false, spacer.isReady(), "Spacer should not be ready right after firing");
+			this.wait(function() {
+					Y.Assert.areEqual(true, spacer.isReady(), "Spacer should be ready following firing and then a pause");
+				},
+				120);
+				
+			spacer.haveFired();
+			Y.Assert.areEqual(false, spacer.isReady(), "Spacer should not be ready right after second firing");
+			this.wait(function() {
+					Y.Assert.areEqual(true, spacer.isReady(), "Spacer should be ready following second firing and then a pause");
+				},
+				120);
+		}
+
+	});
+
+} );
