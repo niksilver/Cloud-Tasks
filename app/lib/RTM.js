7d9feae4a81a40d749c815b34acbda5c818fbae7
Nik <Nik@Westminster>
2009-12-31 17:50:37 +0000

    Removed notes about a potential bug. There isn't one because Javascript is single-threaded.

--- app/lib/RTM.js
+++ app/lib/RTM.js
@@ -330,15 +330,6 @@ RTM.prototype.pushLocalChange = function(task, property, successCallback, failur
 	var method;
 	var parameters;
 	var augmented_success_callback = function(response) {
-		//
-		// -------------------------------------------------
-		// Bug here: Asynchronous pushes mean the wrong
-		// properties get marked not for push. Should
-		// probably use a hash instead of an array.
-		// Also, what does the task list look like after
-		// a deletion has been pushed out?
-		// -------------------------------------------------
-		//
 		task.markNotForPush(property);
 		successCallback(response);
 	}
