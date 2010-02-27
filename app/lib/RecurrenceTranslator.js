3c1af7e1a8ec8ba0ad59124d6368f1e752c96681
Nik <Nik@Westminster>
2010-02-27 22:48:04 +0000

    A bit of refactoring.

--- app/lib/RecurrenceTranslator.js
+++ app/lib/RecurrenceTranslator.js
@@ -63,21 +63,31 @@ var RecurrenceTranslator = {
 			day_part = RecurrenceTranslator.dayCodeToDay[data.BYDAY];
 		}
 		else {
-			day_part = '';
-			var day_codes = data.BYDAY;
-			var max_index = day_codes.length - 1;
-			var index_before_and = max_index - 1;
-			for (var i = 0; i <= max_index; i++) {
-				day_part += RecurrenceTranslator.dayCodeToDay[day_codes[i]];
-				if (i == index_before_and) {
-					day_part += ' and ';
-				}
-				else if (i < index_before_and) {
-					day_part += ", ";
-				}
-			}
+			var days = data.BYDAY.map(function(code) { return RecurrenceTranslator.dayCodeToDay[code] });
+			day_part = RecurrenceTranslator.joinWithCommasAndAnd(days);
 		}
 		
 		return "Every " + day_part;
+	},
+	
+	/**
+	 * Take an array of strings, and return a string in which the strings are joined
+	 * with ", " apart from the last two, which are joined with " and ".
+	 * @param {Object} arr  An array of strings.
+	 */
+	joinWithCommasAndAnd: function(arr) {
+		var out = '';
+		var max_index = arr.length - 1;
+		var index_before_and = max_index - 1;
+		for (var i = 0; i <= max_index; i++) {
+			out += arr[i];
+			if (i == index_before_and) {
+				out += ' and ';
+			}
+			else if (i < index_before_and) {
+				out += ", ";
+			}
+		}
+		return out;
 	}
 }
